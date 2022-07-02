ccdfa71d5ec1f31cfdc7c5f080c842b7cd5f2b6b
lif3ng <lif3ng@icloud.com>
2022-07-02 22:42:45 +0800

    build vue2 lib

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,18 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ["vue", "@cloudbase/js-sdk", "axios"],
+      external: [
+        "vue",
+        "vue2",
+        "@vue/composition-api",
+        "@cloudbase/js-sdk",
+        "axios",
+      ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: "Vue",
+          vue2: "Vue",
           "@cloudbase/js-sdk": "cloudbase",
         },
       },
