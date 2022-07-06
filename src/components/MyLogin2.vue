af72beeb6537ac99c883e368f6156f824132707a
lifeng <294916781@qq.com>
2022-07-06 13:13:31 +0800

    0.0.40

--- src/components/MyLogin2.vue
+++ src/components/MyLogin2.vue
@@ -8,7 +8,7 @@ import { defineComponent, h } from "vue-demi";
 export default defineComponent({
   name: "MyLogin",
   render() {
-    return h("div", "login");
+    return h("div", null, "login");
   },
 });
 </script>
